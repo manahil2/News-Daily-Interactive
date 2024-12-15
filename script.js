bc49f5782e3c6f82c3a6c59d7cb47263afe79269
Manahil Aleem <manahilaleem1@gmail.com>
2024-12-15 18:33:43 +0500

    deploy

---
+++ script.js
@@ -0,0 +1,48 @@
+const readmore1 = document.getElementById("readmore1");
+const extracontent1 = document.getElementById("extracontent1");
+readmore1.onclick = () => {
+    if(extracontent1.style.display === "none"){
+        extracontent1.style.display = "inline";
+        readmore1.textContent = "Read Less";
+    } else {
+        extracontent1.style.display = "none";
+        readmore1.textContent = "Read More";
+    }
+};
+
+const readmore2 = document.getElementById("readmore2");
+const extracontent2 = document.getElementById("extracontent2");
+readmore2.onclick = () => {
+    if(extracontent2.style.display === "none"){
+        extracontent2.style.display = "inline";
+        readmore2.textContent = "Read Less";
+    } else {
+        extracontent2.style.display = "none";
+        readmore2.textContent = "Read More";
+    }
+};
+
+const readmore3 = document.getElementById("readmore3");
+const extracontent3 = document.getElementById("extracontent3");
+readmore3.onclick = () => {
+    if(extracontent3.style.display === "none"){
+        extracontent3.style.display = "inline";
+        readmore3.textContent = "Read Less";
+    } else {
+        extracontent3.style.display = "none";
+        readmore3.textContent = "Read More";
+    }
+};
+
+const readmore4 = document.getElementById("readmore4");
+const extracontent4 = document.getElementById("extracontent4");
+readmore4.onclick = () => {
+    if(extracontent4.style.display === "none"){
+        extracontent4.style.display = "inline";
+        readmore4.textContent = "Read Less";
+    } else {
+        extracontent4.style.display = "none";
+        readmore4.textContent = "Read More";
+    }
+};
+
